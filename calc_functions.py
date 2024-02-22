874a5b76d49f1b75c1fd643173eb948b18019464
Jared Andrews <jared.andrews07@gmail.com>
2024-02-22 17:18:02 -0600

    lint other modules

--- calc_functions.py
+++ calc_functions.py
@@ -1,4 +1,3 @@
-
 import strprofiler as sp
 import pandas as pd
 import random
@@ -7,24 +6,51 @@ from collections import OrderedDict
 import time
 
 
-def single_query(query, str_database, use_amel, three_allele_threshold, query_filter, query_filter_threshold):
- 
-    if query_filter == 'Tanabe':
-        query_filter_name = 'tanabe_score'
-        drop_cols = ['masters_query_score', 'masters_ref_score', 'query_sample', 'n_query_alleles', 'n_reference_alleles']
-    elif query_filter == 'Masters Query':
-        query_filter_name = 'masters_query_score'
-        drop_cols = ['tanabe_score', 'masters_ref_score', 'query_sample', 'n_query_alleles', 'n_reference_alleles']
-    elif query_filter == 'Masters Reference':
-        query_filter_name = 'masters_ref_score'
-        drop_cols = ['tanabe_score', 'masters_query_score', 'query_sample', 'n_query_alleles', 'n_reference_alleles']
-    
+def single_query(
+    query,
+    str_database,
+    use_amel,
+    three_allele_threshold,
+    query_filter,
+    query_filter_threshold,
+):
+
+    if query_filter == "Tanabe":
+        query_filter_name = "tanabe_score"
+        drop_cols = [
+            "masters_query_score",
+            "masters_ref_score",
+            "query_sample",
+            "n_query_alleles",
+            "n_reference_alleles",
+        ]
+    elif query_filter == "Masters Query":
+        query_filter_name = "masters_query_score"
+        drop_cols = [
+            "tanabe_score",
+            "masters_ref_score",
+            "query_sample",
+            "n_query_alleles",
+            "n_reference_alleles",
+        ]
+    elif query_filter == "Masters Reference":
+        query_filter_name = "masters_ref_score"
+        drop_cols = [
+            "tanabe_score",
+            "masters_query_score",
+            "query_sample",
+            "n_query_alleles",
+            "n_reference_alleles",
+        ]
+
     current_time = time.strftime("%H:%M", time.localtime())
 
-    mixed = sp.mixing_check(alleles=query, three_allele_threshold=three_allele_threshold)
+    mixed = sp.mixing_check(
+        alleles=query, three_allele_threshold=three_allele_threshold
+    )
 
     q_out = {
-        "Sample": 'Query-'+current_time,
+        "Sample": "Query-" + current_time,
         "mixed": mixed,
         "query_sample": True,
         "n_shared_markers": nan,
@@ -42,7 +68,9 @@ def single_query(query, str_database, use_amel, three_allele_threshold, query_fi
 
     for sa in str_database.keys():
         r = str_database[sa]
-        scores = sp.score_query(query=query, reference=r, use_amel=use_amel, amel_col="Amelogenin")
+        scores = sp.score_query(
+            query=query, reference=r, use_amel=use_amel, amel_col="Amelogenin"
+        )
 
         # Create dict of scores for each sample comparison.
         samp_out = OrderedDict({"Sample": sa})
@@ -57,28 +85,50 @@ def single_query(query, str_database, use_amel, three_allele_threshold, query_fi
         by=query_filter_name, ascending=False, inplace=True, na_position="first"
     )
 
-    full_samp_out = full_samp_out.round({'tanabe_score': 2, 'masters_query_score': 2, 'masters_ref_score': 2})
+    full_samp_out = full_samp_out.round(
+        {"tanabe_score": 2, "masters_query_score": 2, "masters_ref_score": 2}
+    )
 
-     
+    full_samp_out = full_samp_out[
+        (full_samp_out[query_filter_name] >= query_filter_threshold)
+        | (full_samp_out.index == 0)
+    ]
 
+    full_samp_out.drop(columns=drop_cols, inplace=True)
 
-    full_samp_out = (full_samp_out[(full_samp_out[query_filter_name] >= query_filter_threshold) | (full_samp_out.index == 0)])
-
-    full_samp_out.drop(columns=drop_cols, inplace = True)
-
-    full_samp_out.rename(columns={"mixed": "Mixed Sample", "n_shared_markers": "Shared Markers", "n_shared_alleles": "Shared Alleles", "tanabe_score": "Tanabe Score", "masters_query_score": "Master Query Score", "masters_ref_score": "Master Ref Score"}, inplace = True)
+    full_samp_out.rename(
+        columns={
+            "mixed": "Mixed Sample",
+            "n_shared_markers": "Shared Markers",
+            "n_shared_alleles": "Shared Alleles",
+            "tanabe_score": "Tanabe Score",
+            "masters_query_score": "Master Query Score",
+            "masters_ref_score": "Master Ref Score",
+        },
+        inplace=True,
+    )
 
     return full_samp_out
 
 
-def batch_query(query_df, str_database, use_amel, three_allele_threshold, tan_threshold, mas_q_threshold, mas_r_threshold):
+def batch_query(
+    query_df,
+    str_database,
+    use_amel,
+    three_allele_threshold,
+    tan_threshold,
+    mas_q_threshold,
+    mas_r_threshold,
+):
 
     summaries = []
-    
+
     for s in query_df.keys():
         q = query_df[s]
         # Check for sample mixing.
-        mixed = sp.mixing_check(alleles=q, three_allele_threshold=three_allele_threshold)
+        mixed = sp.mixing_check(
+            alleles=q, three_allele_threshold=three_allele_threshold
+        )
 
         q_out = {
             "Sample": s,
@@ -96,16 +146,16 @@ def batch_query(query_df, str_database, use_amel, three_allele_threshold, tan_th
 
         # Put query sample first.
         samp_comps = [q_out]
- 
+
         for sa in str_database.keys():
             if sa != s:
                 r = str_database[sa]
-                
+
                 try:
                     scores = sp.score_query(query=q, reference=r, use_amel=use_amel)
                 except:
                     return False
-                
+
                 # Create dict of scores for each sample comparison.
                 samp_out = OrderedDict({"Sample": sa})
                 samp_out.update(scores)
@@ -121,34 +171,60 @@ def batch_query(query_df, str_database, use_amel, three_allele_threshold, tan_th
 
         # Generate summary of scores for given sample.
         summ = sp.make_summary(
-            samp_df = full_samp_out,
-            alleles = q,
-            tan_threshold = tan_threshold,
-            mas_q_threshold = mas_q_threshold,
-            mas_r_threshold = mas_r_threshold,
-            mixed = mixed,
-            s_name = s,
+            samp_df=full_samp_out,
+            alleles=q,
+            tan_threshold=tan_threshold,
+            mas_q_threshold=mas_q_threshold,
+            mas_r_threshold=mas_r_threshold,
+            mixed=mixed,
+            s_name=s,
         )
         summaries.append(summ)
     summaries = pd.DataFrame(summaries)
 
-    summaries_out = summaries[['Sample', 'mixed', 'top_hit', 'next_best', 'tanabe_matches', 'masters_query_matches', 'masters_ref_matches']]
+    summaries_out = summaries[
+        [
+            "Sample",
+            "mixed",
+            "top_hit",
+            "next_best",
+            "tanabe_matches",
+            "masters_query_matches",
+            "masters_ref_matches",
+        ]
+    ]
 
-    summaries_ret = summaries_out.rename(columns={"mixed": "Mixed Sample", "top_hit": "Top Match", "next_best": "Next Best Match", "tanabe_matches": "Tanabe Matches", "masters_query_matches": "Master Query Matches", "masters_ref_matches": "Master Ref Matches"})
+    summaries_ret = summaries_out.rename(
+        columns={
+            "mixed": "Mixed Sample",
+            "top_hit": "Top Match",
+            "next_best": "Next Best Match",
+            "tanabe_matches": "Tanabe Matches",
+            "masters_query_matches": "Master Query Matches",
+            "masters_ref_matches": "Master Ref Matches",
+        }
+    )
 
     return summaries_ret
 
 
-
-
-def file_query(query_df, use_amel, three_allele_threshold, tan_threshold, mas_q_threshold, mas_r_threshold):
+def file_query(
+    query_df,
+    use_amel,
+    three_allele_threshold,
+    tan_threshold,
+    mas_q_threshold,
+    mas_r_threshold,
+):
 
     summaries = []
-    
+
     for s in query_df.keys():
         q = query_df[s]
         # Check for sample mixing.
-        mixed = sp.mixing_check(alleles=q, three_allele_threshold=three_allele_threshold)
+        mixed = sp.mixing_check(
+            alleles=q, three_allele_threshold=three_allele_threshold
+        )
 
         q_out = {
             "Sample": s,
@@ -166,7 +242,7 @@ def file_query(query_df, use_amel, three_allele_threshold, tan_threshold, mas_q_
 
         # Put query sample first.
         samp_comps = [q_out]
- 
+
         for sa in query_df.keys():
             if sa != s:
                 r = query_df[sa]
@@ -187,19 +263,38 @@ def file_query(query_df, use_amel, three_allele_threshold, tan_threshold, mas_q_
 
         # Generate summary of scores for given sample.
         summ = sp.make_summary(
-            samp_df = full_samp_out,
-            alleles = q,
-            tan_threshold = tan_threshold,
-            mas_q_threshold = mas_q_threshold,
-            mas_r_threshold = mas_r_threshold,
-            mixed = mixed,
-            s_name = s,
+            samp_df=full_samp_out,
+            alleles=q,
+            tan_threshold=tan_threshold,
+            mas_q_threshold=mas_q_threshold,
+            mas_r_threshold=mas_r_threshold,
+            mixed=mixed,
+            s_name=s,
         )
         summaries.append(summ)
     summaries = pd.DataFrame(summaries)
 
-    summaries_out = summaries[['Sample', 'mixed', 'top_hit', 'next_best', 'tanabe_matches', 'masters_query_matches', 'masters_ref_matches']]
+    summaries_out = summaries[
+        [
+            "Sample",
+            "mixed",
+            "top_hit",
+            "next_best",
+            "tanabe_matches",
+            "masters_query_matches",
+            "masters_ref_matches",
+        ]
+    ]
 
-    summaries_ret = summaries_out.rename(columns={"mixed": "Mixed Sample", "top_hit": "Top Match", "next_best": "Next Best Match", "tanabe_matches": "Tanabe Matches", "masters_query_matches": "Master Query Matches", "masters_ref_matches": "Master Ref Matches"})
+    summaries_ret = summaries_out.rename(
+        columns={
+            "mixed": "Mixed Sample",
+            "top_hit": "Top Match",
+            "next_best": "Next Best Match",
+            "tanabe_matches": "Tanabe Matches",
+            "masters_query_matches": "Master Query Matches",
+            "masters_ref_matches": "Master Ref Matches",
+        }
+    )
 
     return summaries_ret
